761be86e0923435e7f3fe08537718c76ceca0cee
Mohitsainix <msksaini20@gmail.com>
2026-05-26 01:37:11 +0530

    New Drive update

--- backend/routes/driveRoutes.js
+++ backend/routes/driveRoutes.js
@@ -12,6 +12,40 @@ router.get('/', async (req, res) => {
     }
 });
 
+// Create a new drive
+router.post('/', async (req, res) => {
+    try {
+        const { companyName, jobRole, packageLPA, driveDate } = req.body;
+        
+        const User = require('../models/User');
+        const Company = require('../models/Company');
+
+        let company = await Company.findOne({ companyName: new RegExp(`^${companyName}$`, 'i') });
+        if (!company) {
+            let user = await User.findOne({ role: 'company' });
+            if (!user) user = await User.findOne();
+            if (!user) throw new Error("No users found to associate with company");
+            
+            company = await Company.create({
+                companyName,
+                userId: user._id
+            });
+        }
+        
+        const newDrive = new Drive({
+            companyId: company._id,
+            jobRole,
+            packageLPA,
+            lastDate: driveDate
+        });
+        
+        await newDrive.save();
+        res.status(201).json(newDrive);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 // Get drive by ID
 router.get('/:id', async (req, res) => {
     try {
